88a72de5aca4079004865a9df9b8fd462bb2ffcb
samas92i <samuel.92i@hotmail.fr>
2015-08-19 14:33:13 +0200

    Add more app

---
+++ debian7/java7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Created by Samas92i
+#
+# Copyright 2015 @ Samas92i. All rights reserved.
+	
+verify_packet () {
+	if [ `dpkg-query -W --showformat='${Status}\n' $1 2>/dev/null | grep 'install ok installed' | wc -l` -ge 1 ]; then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+if [ $(verify_packet openjdk-7-jre) -eq 1 ]
+then
+	echo "Java 7 est déjà installé !"
+else
+	apt-get install -y openjdk-7-jre
+fi
